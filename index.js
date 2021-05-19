395cf5dc12fcdca5b6c25681dddee6956dc8f285
mmmfang <mae.fang@gmail.com>
2021-05-19 15:31:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
   for (let i=0; i<array.length; i++){
     if (findFn(array[i]) === findFn) {
-      return findFn
+      return array[i]
     }
   } return null
 }
