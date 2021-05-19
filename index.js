c8713d48d6bbbfbbb1ae6a3a2e63812deebe2660
mmmfang <mae.fang@gmail.com>
2021-05-19 15:31:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function keepGoing(array, changeValue, skipValue) {
 // if the value isn't found.
 function findBy(array, findFn) {
   for (let i=0; i<array.length; i++){
-    if (array[i] === findFn) {
+    if (findFn(array[i]) === findFn) {
       return findFn
     }
     if (array[i]!== findFn) {
